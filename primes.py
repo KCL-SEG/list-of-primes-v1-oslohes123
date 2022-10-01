140951748e6ec5dcb2966a106e4f925eeba020fb
Ashley Tyagi <ashleytyagi@hotamil.com>
2022-10-01 06:19:47 +0100

    Write my code

--- primes.py
+++ primes.py
@@ -2,5 +2,27 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    list = []
+    list = [2]
+    divided = False
+    counter = 3
+    primes_found = 1
+    while primes_found < number_of_primes:
+        for y in range(counter//2,counter):
+            if counter % y == 0 and y != 1 and counter != y:
+                divided = True
+                break
+        if not divided:
+            list.append(counter)
+            primes_found+=1
+        divided = False
+        counter+=1
+
+
+
+
+
     return list
+
+
+print(primes(20))
+print(len(primes(20)))
