2447a35e291d4d6719cd356f41d8cfdc03aa4498
Ashley Tyagi <ashleytyagi@hotamil.com>
2022-10-01 08:16:36 +0100

    update with correct maths idea

--- primes.py
+++ primes.py
@@ -1,26 +1,21 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
-
+import math
 def primes(number_of_primes):
     list = [2]
     divided = False
     counter = 3
     primes_found = 1
     while primes_found < number_of_primes:
-        for y in range(counter//2,counter):
+        for y in range(int(math.sqrt(counter)), counter):
             if counter % y == 0 and y != 1 and counter != y:
                 divided = True
                 break
         if not divided:
             list.append(counter)
-            primes_found+=1
+            primes_found += 1
         divided = False
-        counter+=1
-
-
-
-
-
+        counter += 1
     return list
 
 
